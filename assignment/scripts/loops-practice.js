c65129e411aa6809bb5be377f651346485fb5971
Ben Lynch <bdlynch7@gmail.com>
2023-06-21 13:55:53 -0500

    1c for loop

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -21,10 +21,12 @@ console.log('count from 3 to 5');
 // 1.c. TODO: Write a for loop to console.log EVEN numbers from 2 to 10
 //   - Which part of the example loop do you need to change to do this?
 console.log('count even numbers from 2 to 10 (2, 4, 6, 8, 10):');
-
+for (let i=2; i <= 10; i += 2) {
+  console.log(i);
+}
 // 1.d. (STRETCH) TODO: Write a for loop to do a countdown from 5 to 0
 console.log('STRETCH: countdown from 5 to 0');
-
+for ()
 
 // 2. For of loops  
 console.log('---- 2. For Of loop ----');
